c62cf5cf91621d770f58efe1ca24da7a212880b2
Akashraj <akashraj7713@gmail.com>
2018-06-26 16:56:15 +0530

    documenting

--- dict.py
+++ dict.py
@@ -6,11 +6,11 @@ data_item = json.load(open("data.json"))
 
 def find(word):
     words = data_item.keys()
+    word = word.lower()           #case sensitive word eliminate
     if word in words:
         return data_item[word]
     else:
         return "Word you enter does not exist please recheck"
 
 word = input("Please enter a word : ")
-word = word.lower()
 print(find(word))
