016906684affd17edd96caf79689ae073a501ccc
Akashraj <akashraj7713@gmail.com>
2018-06-26 18:26:09 +0530

    adding feature

--- dict.py
+++ dict.py
@@ -1,4 +1,5 @@
 import json
+import difflib
 """with open('data.json') as data_file:
     data_item = json.load(data_file)"""
 
@@ -9,6 +10,8 @@ def find(word):
     word = word.lower()           #case sensitive word eliminate
     if word in words:             #if word in data_item: can also be used
         return data_item[word]
+    elif len(difflib.get_close_matches(word,words,cutoff=0.8))>0:
+        return "Did you mean %s"%difflib.get_close_matches(word,words,cutoff=0.8)[0]
     else:
         return "Word you enter does not exist please recheck"
 
