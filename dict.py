915e2de7dc71db093d1feaa693cc62b7b95ff19e
Akashraj <akashraj7713@gmail.com>
2018-06-26 19:12:16 +0530

    finalizing output

--- dict.py
+++ dict.py
@@ -1,5 +1,5 @@
 import json
-import difflib
+import difflib            #this library is used to compare strings
 """with open('data.json') as data_file:
     data_item = json.load(data_file)"""
 
@@ -11,9 +11,20 @@ def find(word):
     if word in words:             #if word in data_item: can also be used
         return data_item[word]
     elif len(difflib.get_close_matches(word,words,cutoff=0.8))>0:
-        return "Did you mean %s"%difflib.get_close_matches(word,words,cutoff=0.8)[0]
+        yn = input("Did you mean %s(y or n) : "%difflib.get_close_matches(word,words,cutoff=0.8)[0])
+        if yn.lower()=='y' or 'yes':
+            return data_item[difflib.get_close_matches(word,words,cutoff=0.8)[0]]
+        elif yn.lower()== 'n' or 'no':
+            return "I cann't find the word, please recheck"
+        else:
+            return "Sorry,I didn't get you"
     else:
-        return "Word you enter does not exist please recheck"
+        return "I cann't find the word, please recheck"
 
 word = input("Please enter a word : ")
-print(find(word))
+result = find(word)
+if type(result) == list:
+    for item in result:
+        print(item)
+else:
+    print(result)
